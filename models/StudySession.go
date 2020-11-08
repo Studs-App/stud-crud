1c04610919700681c59c8fa8aec41a13e463d3a5
frlzjosh <joshua.magdaleno.472@my.csun.edu>
2020-11-08 09:44:14 -0800

    updating json attributes

--- models/StudySession.go
+++ models/StudySession.go
@@ -7,15 +7,16 @@ import (
 
 type StudySession struct {
 	gorm.Model
-	Title        string         `json:"title"`
-	Subject      string         `json:"subject"`
-	Status       string         `json:"status"`
-	Lattitude    float64        `json:"lattitude"`
-	Longitude    float64        `json:"longitude"`
-	Location     string         `json:"location"`
-	Buds         pq.StringArray `gorm:"type:varchar(64)[]" json:"buds"`
-	Duration     string         `json:"duration"`
-	ScheduledDate string         `json:"scheduled_date "`
-	IsPrivate    bool           `json:"is_private"`
-	ProfileId    int            `json:"profile_id"`
+	Title         string         `json:"title"`
+	Description   string         `json:"description"`
+	Subject       string         `json:"subject"`
+	Status        string         `json:"status"`
+	Lattitude     float64        `json:"lattitude"`
+	Longitude     float64        `json:"longitude"`
+	Location      string         `json:"location"`
+	Buds          pq.StringArray `gorm:"type:varchar(64)[]" json:"buds"`
+	Duration      string         `json:"duration"`
+	ScheduledDate string         `json:"scheduledDate "`
+	IsPrivate     bool           `json:"isPrivate"`
+	ProfileId     int            `json:"profile_id"`
 }
